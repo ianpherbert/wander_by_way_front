1b4405b5985c2b98346db17ec869dd26fb5d7e18
Ian Patrick Herbert <ianpatrickherbert@gmail.com>
2023-01-29 19:12:53 +0100

    linting

--- src/utils/mapTripIcons.ts
+++ src/utils/mapTripIcons.ts
@@ -4,23 +4,23 @@ import {RouteType} from "../graphql/model/globalTypes";
 export function mapTripIcons(type: RouteType): string {
     let iconClass;
     switch (type) {
-        case RouteType.BUS:
-            iconClass = "icofont-bus"
-            break;
-        case RouteType.TRAIN:
-            iconClass = "icofont-train-line"
-            break;
-        case RouteType.PLANE:
-            iconClass = "icofont-airplane"
-            break;
-        case RouteType.BOAT:
-            iconClass = "icofont-sail-boat"
-            break;
-        case RouteType.CAR:
-            iconClass = "icofont-car-alt-4"
-            break
-        default:
-            iconClass = "icofont-travelling"
+    case RouteType.BUS:
+        iconClass = "icofont-bus";
+        break;
+    case RouteType.TRAIN:
+        iconClass = "icofont-train-line";
+        break;
+    case RouteType.PLANE:
+        iconClass = "icofont-airplane";
+        break;
+    case RouteType.BOAT:
+        iconClass = "icofont-sail-boat";
+        break;
+    case RouteType.CAR:
+        iconClass = "icofont-car-alt-4";
+        break;
+    default:
+        iconClass = "icofont-travelling";
     }
-    return iconClass
+    return iconClass;
 }
