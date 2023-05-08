173a1b4f649fe99331c7632eae0239507358de75
Ian Patrick Herbert <ianpatrickherbert@gmail.com>
2023-05-08 10:57:29 +0200

    create cookie policy

--- src/components/cookies/CookieConstent.tsx
+++ src/components/cookies/CookieConstent.tsx
@@ -1,16 +1,41 @@
 import React from "react";
 import {CookieConsent} from "react-cookie-consent";
+import {randomHello} from "../../utils/randomHello";
 
 export const CookieConsentObject = () => {
+
+    const style = {
+        popup: {
+            background: "#2B373B"
+        },
+        link: {
+            color: "white",
+            fontWeight: "bold"
+        },
+        button: {
+            background: "#da4167",
+            color: "#333533",
+            fontSize: "13px",
+            width: "250px",
+            height: "50px",
+            borderRadius: "3px"
+        }
+    };
+
+
     return (
         <CookieConsent
             location="bottom"
             buttonText="Got it!"
             cookieName="cookieConsent"
-            style={{background: "#2B373B"}}
-            buttonStyle={{
-                color: "#4e503b",
-                fontSize: "13px"
-            }}>{"Our website uses cookies🍪 to enhance the user experience"}</CookieConsent>
+            style={style.popup}
+            buttonStyle={style.button}><p>{randomHello()}! Before you explore our website, please be aware that we use
+            cookies and similar
+            technologies to
+            enhance your experience. By clicking &lsquo;Accept&lsquo;, you&lsquo;ll be giving us your ticket to
+            personalize content, and provide the best user experience possible, as described in our <a
+                style={style.link}
+                href={"/privacy"}>Privacy
+                Policy</a>.</p></CookieConsent>
     );
 };
