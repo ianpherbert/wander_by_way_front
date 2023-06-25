d6750d52ed9b3a91d9ff2ce16b9121565d543019
Ian Patrick Herbert <ianpatrickherbert@gmail.com>
2023-06-25 13:04:42 +0200

    refactor: header -> mui

---
+++ src/components/common/header/headerStyle.ts
@@ -0,0 +1,33 @@
+export const headerStyle = {
+    header: {
+        position: "absolute",
+        left: "90px",
+        top: "15px",
+        right: "40px",
+        justifyContent: "space-between",
+        borderBottom: "black 2px solid",
+        width: "90vw"
+    },
+    links: {
+        languageGrid: {
+            display: "flex",
+            justifyContent: "center"
+        },
+        connectionGrid: {
+            borderRight: "black 2px solid",
+            display: "flex", justifyContent: "center"
+        }
+    },
+    langIcon: {
+        height: " 20px",
+        width: " 30px",
+        borderRadius: " 6px",
+        cursor: " pointer",
+        "&:hover": {
+            boxShadow: " 3px 3px 15px -2px rgba(139,190,178,0.68)",
+        },
+        "&:active": {
+            boxShadow: " 3px 3px 15px -2px rgb(139, 190, 178)",
+        }
+    }
+};
--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import "./index.scss";
 import {Languages} from "../../../utils/Languages";
+import {Avatar, Chip, Grid, Link, Typography} from "@mui/material";
+import {headerStyle} from "./headerStyle";
+import {PermIdentity} from "@mui/icons-material";
 
-interface HeaderProps{
+interface HeaderProps {
     connected: boolean,
     username?: string,
     language: Languages
@@ -11,26 +13,37 @@ interface HeaderProps{
 const Header = (props: HeaderProps) => {
 
     const flagLink = props.language == Languages.FR ? "/languageIcons/FR.png" : "/languageIcons/EN.png";
-    return(
-        <div id={"header"}>
-            <a className={"title"} href="/home">
-                <h1>Wander</h1>
-                <h1 className={"by-higlight"}>by</h1>
-                <h1>Way</h1>
-            </a>
 
-            <div className={"connection"}>
-                <div className={"log-in-wrapper"}>
-                    <a className={"log-in"} href={"/login"}>
-                        {props.connected ? props.username || "Your account" : "Log In"}
-                        <i className="icofont-user-alt-7"/>
-                    </a>
-                </div>
-                <div className={"flag-link"}>
-                    <img src={flagLink} alt={props.language == Languages.FR ? "drapeau français" : "british flag"}/>
-                </div>
-            </div>
-        </div>
+    function redirectLogin() {
+        window.location.replace("/login");
+    }
+
+    return (
+        <header>
+            <Grid container sx={headerStyle.header}>
+                <Grid item xs={6}>
+                    <Link href={"/"} underline="none" sx={{color: "black"}}>
+                        <Typography variant="h3" component="h1">Wander By Way</Typography>
+                    </Link>
+                </Grid>
+                <Grid container xs={2} justifyContent="space-evenly"
+                    alignItems="center">
+                    <Grid sx={headerStyle.links.connectionGrid}
+                        justifyContent="center" item xs={6}>
+                        <Chip icon={<PermIdentity/>}
+                            label={props.connected ? props.username || "Your account" : "Log In"}
+                            onClick={redirectLogin}/>
+                    </Grid>
+                    <Grid xs={6} sx={headerStyle.links.languageGrid} item
+                        justifyContent="center">
+                        <Link href={"#"}>
+                            <Avatar src={flagLink}
+                                alt={props.language == Languages.FR ? "drapeau français" : "british flag"}/>
+                        </Link>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </header>
     );
 };
 
