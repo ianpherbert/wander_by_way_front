c761248c11b6b3912e8a99bb9e7301a33a255f8f
Ian Patrick Herbert <ianpatrickherbert@gmail.com>
2023-06-25 13:16:56 +0200

    feature: mui loader

--- src/components/common/loader/index.tsx
+++ src/components/common/loader/index.tsx
@@ -1,24 +1,14 @@
 import React from "react";
 import "./index.scss";
+import {CircularProgress, Container} from "@mui/material";
+import {loaderContainerStyle} from "./loaderStyle";
 
-interface LoaderProps{
-    solid?: boolean;
-    size?: number;
-}
+const Loader = () => {
 
-const Loader=(props: LoaderProps)=>{
     return (
-        <div id={"loader"} className={props.solid ? "solid" : ""} style={{height: `${props.size}px` || "80px"}}>
-            <div className={`chevrons`}>
-                <div className={"chevron teal"}/>
-                <div className={"chevron yellow"}/>
-                <div className={"chevron red"}/>
-                <div className={"chevron_spacer"}/>
-                <div className={"chevron teal"}/>
-                <div className={"chevron yellow"}/>
-                <div className={"chevron red"}/>
-            </div>
-        </div>
+        <Container sx={loaderContainerStyle}>
+            <CircularProgress color={"success"}/>
+        </Container>
     );
 };
 
